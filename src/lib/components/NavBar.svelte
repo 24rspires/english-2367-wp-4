9975724c3f23000e9344e3dd1c2eb3da6b7f4c66
Riley Spires <quoopiecoding@gmail.com>
2024-04-28 21:26:00 -0400

    Updated Navbar for better visibility and coloring

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import ModeToggle from './ModeToggle.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import NavBarButton from './NavBarButton.svelte';
 </script>
 
 <nav
-	class="sticky left-0 right-0 top-0 m-auto flex w-screen items-center justify-between bg-gray-100 p-4 text-lg dark:bg-zinc-900"
+	class="sticky left-0 right-0 top-0 m-auto flex w-screen items-center justify-between bg-gray-100 p-4 text-lg dark:bg-zinc-900/50"
 >
 	<div class="flex items-center">
-		<a href="/">Riley Spires</a>
+		<NavBarButton href="/">Riley Spires</NavBarButton>
 	</div>
 	<div class="row ml-0 flex items-center justify-around gap-4">
-		<a href="/intro">Intro</a>
-		<a href="/growth">Growth</a>
-		<a href="/donts">Do(n't)s</a>
-		<a href="/conclusion">Conclusion</a>
+		<NavBarButton href="/intro">Intro</NavBarButton>
+		<NavBarButton href="/growth">Growth</NavBarButton>
+		<NavBarButton href="/donts">Do(n't)s</NavBarButton>
+		<NavBarButton href="/conclusion">Conclusion</NavBarButton>
 		<ModeToggle />
 	</div>
 </nav>
---
+++ src/lib/components/NavBarButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+
+	export let href: string;
+</script>
+
+<Button variant="outline" on:click={() => (window.location.href = href)}>
+	<slot />
+</Button>
