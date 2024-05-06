ad1fccb824179ae21e374de1d2786cfb6fba4c59
Riley Spires <24rspires@olsd.us>
2024-05-06 09:15:28 -0400

    Please vercel

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 import * as fs from 'fs';
 
 export const load: PageServerLoad = async ({}) => {
-	const intro = fs.readFileSync('./static/intro.txt', 'utf-8').split('\n');
+	const intro = fs.readFileSync('./src/lib/static/intro.txt', 'utf-8').split('\n');
 
 	let wordCount = 0;
 
--- src/routes/growth/+page.server.ts
+++ src/routes/growth/+page.server.ts
@@ -2,8 +2,8 @@ import type { PageServerLoad } from '../$types';
 import * as fs from 'fs';
 
 export const load: PageServerLoad = async () => {
-	const writerGrowths = fs.readFileSync('./static/growth-writer.txt', 'utf-8').split('\n');
-	const editorGrowths = fs.readFileSync('./static/growth-editor.txt', 'utf-8').split('\n');
+	const writerGrowths = fs.readFileSync('./src/lib/static/growth-writer.txt', 'utf-8').split('\n');
+	const editorGrowths = fs.readFileSync('./src/lib/static/growth-editor.txt', 'utf-8').split('\n');
 
 	let writerWordCount = 0;
 	let editorWordCount = 0;
