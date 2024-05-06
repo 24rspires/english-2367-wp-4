c600dec7398e46788fd2efba106166955879b58e
Riley Spires <24rspires@olsd.us>
2024-05-06 08:19:09 -0400

    Intro page done

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import type { PageServerLoad } from './$types';
+import * as fs from 'fs';
+
+export const load: PageServerLoad = async ({}) => {
+	const intro = fs.readFileSync('./static/intro.txt', 'utf-8').split('\n');
+
+	let wordCount = 0;
+
+	intro.forEach((line: string) => {
+		wordCount += line.split(/\w+/g).length;
+	});
+
+	return { intro, wordCount };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
+	import { Button } from "$lib/components/ui/button"; 
+
+	export let data;
+
+	let shownCount = "Reveal word count";
+
+	const { intro, wordCount } = data;
+
+	const showWordCount = () => {
+		shownCount = `${wordCount} words`;
+	};
 </script>
 
 <main class="flex flex-col items-center m-4 h-screen">
-	<h1 class="text-3xl font-bold">
+	<h1 class="text-3xl font-bold m-4 mb-2">
 		Writing Project #4
 	</h1>
+	<h1 class="text-xl font-semibold m-4 mb-16">
+		by Riley Spires
+	</h1>
+
+	<div class="flex flex-col items-center text-center max-w-[35%]">
+		{#each intro as line}
+			<p class="text-md">{line}</p>
+		{/each}
+		<Button variant="outline" on:click={showWordCount} class="m-8"> 
+			<p class="text-s">
+				{shownCount}
+			</p>
+		</Button>
+	</div>
 </main>
